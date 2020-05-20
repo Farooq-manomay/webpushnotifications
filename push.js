0d2adea772a7d87119892c2c3d447d9dbc0252dc
unofficialfarooqsheikh <57211933+unofficialfarooqsheikh@users.noreply.github.com>
2020-05-20 16:49:19 +0530

    Tweaking the product

--- push.js
+++ push.js
@@ -1,17 +1,25 @@
-var push = require('web-push');
-
-let vapidkeys = {
-    publicKey: 'BA9y7TXQVGodMg9UvIamvB19lYb-ndgDyakDcm6dKNTKNW_-zMsbQpRzN5hEor4VsbfpZv3cEcI-U-t2ie_lJW0',
-    privateKey: 'XTIqGhOhdVBKfJk5Km5hRssB6E2eka-e3DvbAdLU06Y'
+export default function pusher(){
+  var push = require('web-push');
+  
+  let vapidkeys = {
+      publicKey: 'BA9y7TXQVGodMg9UvIamvB19lYb-ndgDyakDcm6dKNTKNW_-zMsbQpRzN5hEor4VsbfpZv3cEcI-U-t2' +
+              'ie_lJW0',
+      privateKey: 'XTIqGhOhdVBKfJk5Km5hRssB6E2eka-e3DvbAdLU06Y'
   }
-
-push.setVapidDetails('mailto:test@code.co.in', vapidkeys.publicKey, vapidkeys.privateKey)
-
-let sub = {
-  "endpoint":"https://fcm.googleapis.com/fcm/send/eLAPhkPYMSw:APA91bEKyIKMB10IAj8tsBSt5sTJzMm2JIK7tK2QR9LrJY9pt-udijEcMFOZuZhOkSlzOlUfHkvEVFQjESlVlUJh-7kZrUarVMKDlPnKtE_Uam4MnEBBpw2pBEGwj3c3REmvyOsHdPd7",
-  "expirationTime":null,
-  "keys":{"p256dh":"BCBSgdnFIJhslTlZQQAKiRAFjhvQ9eBr4bXmN1dzkVIRUMCjpYgX942gpB_9FjB9a3DWqL9pmlR7DnM8jbk217o"},
-  "auth":"zHXltPDSOeAKDmMjLzqGvg"
-};
-
-push.sendNotification(sub, 'test message')
+  
+  push.setVapidDetails('mailto:test@code.co.in', vapidkeys.publicKey, vapidkeys.privateKey)
+  
+  let sub = {
+      "endpoint": "https://fcm.googleapis.com/fcm/send/eLAPhkPYMSw:APA91bEKyIKMB10IAj8tsBSt5sTJzMm2" +
+              "JIK7tK2QR9LrJY9pt-udijEcMFOZuZhOkSlzOlUfHkvEVFQjESlVlUJh-7kZrUarVMKDlPnKtE_Uam4M" +
+              "nEBBpw2pBEGwj3c3REmvyOsHdPd7",
+      "expirationTime": null,
+      "keys": {
+          "p256dh": "BCBSgdnFIJhslTlZQQAKiRAFjhvQ9eBr4bXmN1dzkVIRUMCjpYgX942gpB_9FjB9a3DWqL9pmlR7DnM8" +
+                  "jbk217o",
+          "auth": "zHXltPDSOeAKDmMjLzqGvg"
+      }
+  };
+  
+  push.sendNotification(sub, 'test message')
+}
